2ba6900d05efc4e86f50477e154caae7058c6aae
raghub <raghu.badde@nukkadshops.com>
2018-09-05 02:20:40 +0530

    Initial Commit

--- SmartHome/src/main/java/com/smart/digihome/SplashActivity.java
+++ SmartHome/src/main/java/com/smart/digihome/SplashActivity.java
@@ -1,21 +1,36 @@
 package com.smart.digihome;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+
+import customviews.GridRecyclerView;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+       // setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
+                finish();
+                startActivity(intent);
+                //overridePendingTransition(R.anim.fadein, R.anim.fadeout);
 
+            }
+        },3000);
 
-
-    }
+       }
 
 }
